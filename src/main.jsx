e16b51cfcb93adbc28a5eb549185218276bbb8a0
efdourado <ed320819@gmail.com>
2025-03-16 11:37:57 -0300

    Code cleanup and minor fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TaskPage from "./pages/TaskPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([ {
+    path: "/",
+    element: <App />,
+  }, {
+    path: "/task",
+    element: <TaskPage />,
+}, ]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
